ca64cf77194ef88b360c9ee428e06471ff9249e9
pandix73 <conan11conan112004@gmail.com>
2016-10-04 20:52:06 +0800

    finish

--- 103062135_lab1/resizedImage_bilinear.m
+++ 103062135_lab1/resizedImage_bilinear.m
@@ -3,6 +3,7 @@ function [resultedImage] = resizedImage_bilinear(originalImage, scalingFactor)
 tic % count runtime
 
 % some variables we need
+finetune = 1e-5;
 ori_h = size(originalImage, 1);
 ori_w = size(originalImage, 2);
 new_h = floor(ori_h * scalingFactor);
@@ -17,21 +18,21 @@ w_factor = (ori_w-1) / (new_w-1);
 for i = 1:new_h
     % find the coordinate point in original image
     x = h_factor * (i-1) + 1;
-    x1 = floor(x);
-    x2 = ceil(x);
+    x1 = floor(x + finetune);
+    x2 = ceil(x - finetune);
     
     for j = 1:new_w
         % find the coordinate point in original image
         y = w_factor * (j-1) + 1;
-        y1 = floor(y);
-        y2 = ceil(y);
-                
+        y1 = floor(y + finetune);
+        y2 = ceil(y - finetune);
+        
         % use the interpolation method
         a = x - x1;
         b = y - y1;
         if(new_c == 1) % grayscale
             x1y1 = originalImage(x1, y1);
-            x1y2 = originalImage(x1, y2);
+            x1y2 = originalImage(x1, y2);        
             x2y1 = originalImage(x2, y1);
             x2y2 = originalImage(x2, y2);
             resultedImage(i, j) = (1-a) * ((1-b)*x1y1 + b*x1y2) + a * ((1-b)*x2y1 + b*x2y2);
