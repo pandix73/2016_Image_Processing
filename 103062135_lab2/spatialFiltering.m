69921d95701e1555e432cc22e0cc7047b79c1431
pandix73 <conan11conan112004@gmail.com>
2016-10-27 23:13:11 +0800

    add lab2 bonus

--- 103062135_lab2/spatialFiltering.m
+++ 103062135_lab2/spatialFiltering.m
@@ -8,6 +8,8 @@ mh_trans = int16(mh/2);
 mw_trans = int16(mw/2);
 output = zeros(h, w, 'single');
 
+% pad with zeros
+
 for i = 1 : h
     for j = 1 : w
         % transform the index
@@ -22,6 +24,50 @@ for i = 1 : h
         end    
     end
 end
+
+
+% ignore the boundary
+
+% for i = 1+mh_trans : h-mh_trans
+%     for j = 1+mh_trans : w-mh_trans
+%         % transform the index
+%         for k = 1-mh_trans : mh-mh_trans
+%             for l = 1-mw_trans : mw-mw_trans              
+%                 % do the sigma
+%                 output(i, j) = output(i, j) + input(i+k, j+l) * mask(k+mh_trans, l+mw_trans);                
+%             end
+%         end    
+%     end
+% end
+
+
+% mirroring
+
+% for i = 1 : h
+%     for j = 1 : w
+%         % transform the index
+%         for k = 1-mh_trans : mh-mh_trans
+%             for l = 1-mw_trans : mw-mw_trans              
+%                 if(i+k <= 0) 
+%                     x = 1; 
+%                 elseif(i+k > h) 
+%                     x = h;
+%                 else
+%                     x = i+k;
+%                 end
+%                 
+%                 if(j+l <= 0) 
+%                     y = 1; 
+%                 elseif(j+l > w) 
+%                     y = w;
+%                 else
+%                     y = j+l;
+%                 end
+%                 output(i, j) = output(i, j) + input(x, y) * mask(k+mh_trans, l+mw_trans);
+%             end
+%         end    
+%     end
+% end
 
 
 end
